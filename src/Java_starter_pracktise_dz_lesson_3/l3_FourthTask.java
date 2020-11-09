4b2d680f65b6b9430fd276c251e6337d907b4402
Alex Potter <nekospoon228@gmail.com>
2020-11-09 14:52:36 +0300

    l3_FourthTask completed

--- src/Java_starter_pracktise_dz_lesson_3/l3_FourthTask.java
+++ src/Java_starter_pracktise_dz_lesson_3/l3_FourthTask.java
@@ -1,7 +1,28 @@
 package Java_starter_pracktise_dz_lesson_3;
 
+import java.util.Scanner;
+
 public class l3_FourthTask {
 public static void main(String[]args){
-    System.out.println("git_test");
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("Введите ширину прямоугольника:");
+    int width= scanner.nextInt();
+    System.out.println("Введите высоту прямоугольника:");
+    int height= scanner.nextInt();
+        for (int zero=0;zero<width;zero++){
+            System.out.print("*");
+        }
+    System.out.println();
+        for (int zero=2;zero<height;zero++){
+            System.out.print("*");
+            for (int space=width-2;space>0;space--){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            System.out.println();
+        }
+    for (int zero=0;zero<width;zero++) {
+        System.out.print("*");
+    }
 }
 }
