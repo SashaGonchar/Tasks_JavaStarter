7a4a85c6f22403054d5c2409e0c43fe7f82c145b
Alex Potter <nekospoon228@gmail.com>
2020-11-15 23:46:54 +0300

    adding lesson 6 tasks and creating clases

---
+++ src/Java_starter_practice_lesson_6/l6_SecondTask.java
@@ -0,0 +1,4 @@
+package Java_starter_practice_lesson_6;
+//Написать свой вариант метода Arrays.toString() для int[].
+public class l6_SecondTask {
+}
---
+++ src/Java_starter_practice_lesson_6/l6_ThirdTask.java
@@ -0,0 +1,5 @@
+package Java_starter_practice_lesson_6;
+//Ввести с консоли число в бинарном формате. Перевести его в
+//десятичный и вывести на экран (“10” -> 2).
+public class l6_ThirdTask {
+}
