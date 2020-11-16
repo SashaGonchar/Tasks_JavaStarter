86745c0ff0aa9a6a6f404189861a6a49486fd465
Alex Potter <nekospoon228@gmail.com>
2020-11-16 18:15:55 +0300

    completed third task lesson six

--- src/Java_starter_practice_lesson_6/l6_ThirdTask.java
+++ src/Java_starter_practice_lesson_6/l6_ThirdTask.java
@@ -1,5 +1,16 @@
 package Java_starter_practice_lesson_6;
+
+import java.util.Scanner;
+
 //Ввести с консоли число в бинарном формате. Перевести его в
 //десятичный и вывести на экран (“10” -> 2).
 public class l6_ThirdTask {
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число в двоичном формате:");
+        String num=scan.nextLine();
+        int bin_to_dem = Integer.parseInt(num , 2);
+        System.out.println("Ваше число в двоичном виде: "+bin_to_dem);
+
+    }
 }
