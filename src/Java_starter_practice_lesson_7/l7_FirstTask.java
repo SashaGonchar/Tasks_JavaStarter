ce0d26c183e81afdcf596e0ae09d7d37a65b3032
Alex Potter <nekospoon228@gmail.com>
2020-11-16 21:48:13 +0300

    added tasks for next day

---
+++ src/Java_starter_practice_lesson_7/l7_FirstTask.java
@@ -0,0 +1,7 @@
+package Java_starter_practice_lesson_7;
+//Создайте консольный «текстовый редактор» с возможностью
+//сохранения набранного текста в файл.
+public class l7_FirstTask {
+    public static void main(String [] args){
+    }
+}
---
+++ src/Java_starter_practice_lesson_7/l7_SecondTask.java
@@ -0,0 +1,5 @@
+package Java_starter_practice_lesson_7;
+//Напишите метод для сохранения в текстовый файл
+//двухмерного массива целых чисел
+public class l7_SecondTask {
+}
