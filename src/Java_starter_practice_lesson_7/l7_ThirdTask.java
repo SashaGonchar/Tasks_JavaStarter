23c322df4969bf710bef660c18437d4e2ad36123
Alex Potter <nekospoon228@gmail.com>
2020-12-03 17:38:10 +0300

    Completed third task of lesson seven

--- src/Java_starter_practice_lesson_7/l7_ThirdTask.java
+++ src/Java_starter_practice_lesson_7/l7_ThirdTask.java
@@ -1,6 +1,34 @@
 package Java_starter_practice_lesson_7;
+
+import java.io.File;
+import java.util.Scanner;
+
 //Реализуйте метод который выведет на экран список всех
 //каталогов которые «лежат» в каталоге который будет
 //параметром этого метода.
 public class l7_ThirdTask {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите абсолютный путь к каталогу,содержимые каталоги которого, хотите посмотреть:");
+        String folderLocation = scan.nextLine();
+        folderList(folderLocation);
+    }
+
+    static void folderList(String nameFolder) {
+        File folder = new File(nameFolder);
+        File[] listFold = folder.listFiles();
+        for (int i = 0; i < listFold.length; i++) {
+            System.out.println(isItDirectory(listFold[i]));
+        }
+    }
+    static String isItDirectory(File file) {
+        String nameFile = file.getName();
+        String res = "";
+        String type = "Directory";
+        if (file.isDirectory()) {
+            res = String.format("%-10.10s\t%-10.10s", nameFile, type);
+        }
+        return res;
+    }
 }
+
